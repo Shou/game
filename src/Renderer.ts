fa16c1e662eef48e7fbf53368f1eef6795335b5d
Benedict Aas <x+g@shou.io>
2020-06-23 22:52:05 +0100

    we about to change lights to a vec3(x, y, radius);

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -21,36 +21,91 @@ const clamp: clamp = (a, n, b) => max(a, min(n, b))
 type Shape = any
 type Graphics = any
 
+interface Uniform {
+  readonly index: number
+  readonly reference: Array<any>
+}
+
 interface Elem {
   shape: Shape
   graphics: Graphics
+  color: number
+  readonly uniform?: Uniform
 }
 
-interface Entity extends Elem {
+interface Entity extends Required<Elem> {
   velocity: Coord
   airborne: boolean
 }
 
 
-type mkElem = (shape: Shape, color: number) => Elem
-const mkElem: mkElem = (shape, color) => {
+type mkElem = (app: any, shape: Shape, color: number) => Elem
+const mkElem: mkElem = (app, shape, color) => {
   const graphics: any = new Pixi.Graphics()
+  graphics.x = shape.x
+  graphics.y = shape.y
   graphics.beginFill(color, 1)
-  graphics.drawShape(shape)
+  graphics.drawShape({ ...shape, x: 0, y: 0 })
   graphics.endFill()
+
+  app.stage.addChild(graphics)
 
   return {
     shape,
     graphics,
+    color,
   }
 }
 
-type mkEntity = (shape: Shape, color: number) => Entity
-const mkEntity: mkEntity = (shape, color) => {
+type Uniforms = { [key: string]: Array<number> }
+
+type setUniform = <R extends Uniforms>(
+  uniforms: R,
+  key: keyof R,
+  index: number,
+  values: Array<any>,
+) => void
+const setUniform: setUniform = (uniforms, key, index, values) => {
+  for (let i = 0; i < values.length; i++) {
+    uniforms[key][index + i] = values[i]
+  }
+}
+
+type mkEntity = (app: any, shape: Shape, color: number, uniform: Array<number>) => Entity
+const mkEntity: mkEntity = (app, shape, color, uniform) => {
+  const index = uniform.length
+  const points = getRectVertices(shape, app.view.width, app.view.height)
+
+  for (let i = 0; i < points.length; i++) {
+    uniform[index + i] = points[i]
+  }
+
   return {
-    ...mkElem(shape, color),
+    ...mkElem(app, shape, color),
+    uniform: {
+      index,
+      reference: uniform,
+    },
     velocity: { x: 0, y: 0 } as Coord,
     airborne: false,
+  }
+}
+
+type mkLight = (app: any, shape: Shape, color: number, uniform: Array<number>) => Elem
+const mkLight: mkLight = (app, shape, color, uniform) => {
+  const index = uniform.length
+  const elem = mkElem(app, shape, color)
+
+  uniform[index] = elem.shape.x / app.view.width
+  uniform[index + 1] = elem.shape.y / app.view.height
+  uniform[index + 2] = shape.radius
+
+  return {
+    ...elem,
+    uniform: {
+      index,
+      reference: uniform,
+    }
   }
 }
 
@@ -170,6 +225,11 @@ const collide: collide = (entity, collidables) => {
 
     // XXX remove always false predicate
     if (containment.some(id => id)) {
+      entity.velocity.y = min(0, v.y) as Y
+      move(entity, s.x, shape.y - s.height)
+      entity.airborne = false
+      return
+
       const ecrx = clamp(-cx * rx, cos(aFrom) * rx, cx * rx) + mx
       const ecry = clamp(-cy * ry, sin(aFrom) * ry, cy * ry) + my
 
@@ -224,7 +284,7 @@ const collide: collide = (entity, collidables) => {
   }
 }
 
-type move = (entity: Entity, x: number, y: number) => void
+type move = (entity: Elem, x: number, y: number) => void
 const move: move = (entity, x, y) => {
   entity.graphics.x = clamp(0, x, 64 * 16)
   entity.graphics.y = max(0, y % (64 * 12))
@@ -266,10 +326,6 @@ const keybindings: keybindings = () => {
   })
 }
 
-const bg = () => {
-  // TODO
-}
-
 let debug: (f: (block: any) => void) => void
   = null as any
 
@@ -278,7 +334,7 @@ export const main: main = (view, pausedState) => {
   const app = new Pixi.Application({
     width: 64 * 16,
     height: 64 * 12,
-    antialias: false,
+    antialias: true,
     transparent: false,
     resolution: 1,
     view,
@@ -286,85 +342,81 @@ export const main: main = (view, pausedState) => {
 
   keybindings()
 
+  // We should pass along color with the rects... mat3?
+  // [ left, top, right,
+  //   bottom, 0, alpha,
+  //   r, g, b ]
+  // We can use middle as glossiness... maybe
   const uniforms = {
       rects: [] as Array<number>,
       lights: [] as Array<number>,
   }
 
-  const debugBlock = mkElem(new Pixi.Rectangle(0, 0, 100, 100), 0xFF0000)
+  const debugBlock = mkElem(app, new Pixi.Rectangle(0, 0, 100, 100), 0xFF0000)
   debugBlock.graphics.alpha = 0.5
   debug = f => f(debugBlock.graphics)
-  app.stage.addChild(debugBlock.graphics)
 
   const structures: Array<Elem> = []
   const entities: Array<Entity> = []
 
   const bgShape = new Pixi.Rectangle(0, 0, app.view.width, app.view.height)
-  const bgBlock: Elem = mkElem(bgShape, 0x8C8888)
-  app.stage.addChild(bgBlock.graphics)
+  const bgBlock: Elem = mkElem(app, bgShape, 0x8C8888)
 
   const floorShape = new Pixi.Rectangle(0, app.view.height - 64, app.view.width, 64)
-  const floorBlock: Elem = mkElem(floorShape, 0xAFAAAA)
-  structures.push(floorBlock)
-  app.stage.addChild(floorBlock.graphics)
+  const floorElem: Elem = mkElem(app, floorShape, 0xAFAAAA)
+  structures.push(floorElem)
   uniforms.rects = [
     ...uniforms.rects,
     ...getRectVertices(floorShape, app.view.width, app.view.height),
   ]
 
   const platformShape = new Pixi.Rectangle(64 * 2, 64 * 8, 64 * 7, 64)
-  const platformBlock: Elem = mkElem(platformShape, 0xAFAAAA)
-  structures.push(platformBlock)
-  app.stage.addChild(platformBlock.graphics)
+  const platform: Elem = mkElem(app, platformShape, 0xAFAAAA)
+  structures.push(platform)
   uniforms.rects = [
     ...uniforms.rects,
     ...getRectVertices(platformShape, app.view.width, app.view.height)
   ]
 
   const wallShape = new Pixi.Rectangle(64 * 8, 64 * 2, 64, 64 * 4)
-  const wallBlock: Elem = mkElem(wallShape, 0xAFAAAA)
-  structures.push(wallBlock)
-  app.stage.addChild(wallBlock.graphics)
+  const wall: Elem = mkElem(app, wallShape, 0xAFAAAA)
+  structures.push(wall)
   uniforms.rects = [
     ...uniforms.rects,
     ...getRectVertices(wallShape, app.view.width, app.view.height)
   ]
 
-  const balloonShape: Shape = new Pixi.Circle(64 * 11, 64 * 7, 64)
-  const balloonElem: Elem = mkElem(balloonShape, 0xAA1111)
-  structures.push(balloonElem)
-  app.stage.addChild(balloonElem.graphics)
-
-  const lightShape: Shape = new Pixi.Circle(app.view.width * 0.5, 48, 32)
-  const lightElem: Elem = mkElem(lightShape, 0xFFFFFF)
-  structures.push(lightElem)
-  app.stage.addChild(lightElem.graphics)
-  uniforms.lights = [
-    ...uniforms.lights,
-    lightShape.x / app.view.width,
-    lightShape.y / app.view.height,
+  const floatyboiShape = new Pixi.Rectangle(64 * 11, 64 * 7, 128, 128)
+  const floatyboi: Elem = mkElem(app, floatyboiShape, 0xAFAAAA)
+  structures.push(floatyboi)
+  uniforms.rects = [
+    ...uniforms.rects,
+    ...getRectVertices(floatyboiShape, app.view.width, app.view.height)
   ]
 
-  const lampShape: Shape = new Pixi.Circle(app.view.width - 48, app.view.height * 0.5, 32)
-  const lampElem: Elem = mkElem(lampShape, 0xFFFFFF)
-  structures.push(lampElem)
-  app.stage.addChild(lampElem.graphics)
-  uniforms.lights = [
-    ...uniforms.lights,
-    lampShape.x / app.view.width,
-    lampShape.y / app.view.height,
-  ]
+  //const balloonShape: Shape = new Pixi.Circle(64 * 11, 64 * 7, 64)
+  //const balloonElem: Elem = mkElem(balloonShape, 0xAA1111)
+  //structures.push(balloonElem)
+  //app.stage.addChild(balloonElem.graphics)
+
+  const lightShape: Shape = new Pixi.Circle(app.view.width * 0.5, 8 * 64 - 32, 32)
+  const light: Elem = mkLight(app, lightShape, 0xFFFFFF, uniforms.lights)
+  structures.push(light)
+
+  const lampShape: Shape
+    = new Pixi.Circle(app.view.width - 48, app.view.height * 0.5, 32)
+  const lamp: Elem = mkLight(app, lampShape, 0xFFFFFF, uniforms.lights)
+  structures.push(lamp)
+
+  const floorLightShape: Shape
+    = new Pixi.Circle(128, app.view.height - 128, 32)
+  const floorLight: Elem = mkLight(app, floorLightShape, 0xFFFFFF, uniforms.lights)
+  structures.push(floorLight)
 
   const playerShape
     = new Pixi.Rectangle(app.view.width * 0.5, app.view.height * 0.5, 64, 64)
-  const player = mkEntity(playerShape, 0xEFE8E8)
+  const player = mkEntity(app, playerShape, 0xEFE8E8, uniforms.rects)
   entities.push(player)
-  app.stage.addChild(player.graphics)
-  const playerUniformIndex: number = uniforms.rects.length
-  uniforms.rects = [
-    ...uniforms.rects,
-    ...getRectVertices(playerShape, app.view.width, app.view.height)
-  ]
 
   const vertexLight = Shader.vertexLight()
   const fragmentLight = Shader.fragmentLight(
@@ -378,7 +430,9 @@ export const main: main = (view, pausedState) => {
   app.ticker.autoStart = false
 
   // NOTE We can use app.ticker.start/stop() for pausing
-  app.ticker.add((lagDelta: number) => {
+  app.ticker.add(() => {
+    const time = performance.now()
+
     for (const entity of entities) {
       accelerate(entity)
       collide(entity, structures)
@@ -386,8 +440,20 @@ export const main: main = (view, pausedState) => {
 
       const vsPlayer = getRectVertices(playerShape, app.view.width, app.view.height)
       for (let i = 0; i < 4; i++) {
-        uniforms.rects[playerUniformIndex + i] = vsPlayer[i]
+        uniforms.rects[player.uniform.index + i] = vsPlayer[i]
       }
+
+      move(lamp, 12 * 64 + cos(time * 0.001) * 128, 256 + sin(time * 0.001) * 128)
+      setUniform(uniforms, "lights", 3, [
+        lamp.shape.x / app.view.width,
+        lamp.shape.y / app.view.height,
+      ])
+
+      move(floorLight, app.view.width * 0.5 + cos(time * 0.001) * 8 * 64, floorLight.shape.y)
+      setUniform(uniforms, "lights", 6, [
+        floorLight.shape.x / app.view.width,
+        floorLight.shape.y / app.view.height,
+      ])
     }
   })
 
--- src/Shader.ts
+++ src/Shader.ts
@@ -36,59 +36,295 @@ uniform sampler2D uSampler;
 vec2 lineIntersect(vec2 p1, vec2 p2, vec2 p3, vec2 p4) {
   float divisor = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x);
   if (divisor == 0.0) {
-    return vec2(-1, -1);
+    return vec2(-1.0, -1.0);
   }
   float t = ((p1.x - p3.x) * (p3.y - p4.y) - (p1.y - p3.y) * (p3.x - p4.x)) / divisor;
-  float u = ((p1.x - p2.x) * (p1.y - p3.y) - (p1.y - p2.y) * (p1.x - p3.x)) / divisor;
+  float u = -((p1.x - p2.x) * (p1.y - p3.y) - (p1.y - p2.y) * (p1.x - p3.x)) / divisor;
 
-  if (0.0 <= t && t <= 1.0 || 0.0 <= u && u <= 1.0) {
-    return vec2(-1, -1);
+  if (0.0 <= t && t <= 1.0 && 0.0 <= u && u <= 1.0) {
+    return vec2(p1.x + t * (p2.x - p1.x), p1.y + t * (p2.y - p1.y));
   }
 
-  return vec2(p1.x + t * (p2.x - p1.x), p1.y + t * (p2.y - p1.y));
+  return vec2(-1.0, -1.0);
+}
+
+// TODO check if angle can actually hit the side
+vec3 lineRectIntersection(float angle, vec2 p1, vec2 p2, vec4 rect) {
+  // TODO Maybe we should precompute this
+  vec2 normal = vec2(cos(angle), sin(angle));
+
+  vec2 isTop = lineIntersect(p1, p2, rect.xy, rect.zy);
+  if (isTop.x != -1.0) {
+    vec2 refl = normal * vec2(1.0, -1.0);
+    float reflAngle = atan(refl.y, refl.x);
+    return vec3(isTop, reflAngle);
+  }
+
+  vec2 isRight = lineIntersect(p1, p2, rect.zy, rect.zw);
+  if (isRight.x != -1.0) {
+    vec2 refl = normal * vec2(-1.0, 1.0);
+    float reflAngle = atan(refl.y, refl.x);
+    return vec3(isRight, reflAngle);
+  }
+
+  vec2 isBottom = lineIntersect(p1, p2, rect.xw, rect.zw);
+  if (isBottom.x != -1.0) {
+    vec2 refl = normal * vec2(1.0, -1.0);
+    float reflAngle = atan(refl.y, refl.x);
+    return vec3(isBottom, reflAngle);
+  }
+
+  vec2 isLeft = lineIntersect(p1, p2, rect.xy, rect.xw);
+  if (isLeft.x != -1.0) {
+    vec2 refl = normal * vec2(-1.0, 1.0);
+    float reflAngle = atan(refl.y, refl.x);
+    return vec3(isLeft, reflAngle);
+  }
+
+  return vec3(-1.0);
+}
+
+vec2 closestVertexToAngle(vec4 cs, float a) {
+  vec2 acc = vec2(-1.0, -1.0);
+  float smallest = 3.141592653589793;
+
+  float tl = abs(atan(cs.y, cs.x) - a);
+  if (tl < smallest) {
+    smallest = tl;
+    acc = cs.xy;
+  }
+  float tr = abs(atan(cs.y, cs.z) - a);
+  if (tr < smallest) {
+    smallest = tr;
+    acc = cs.zy;
+  }
+  float br = abs(atan(cs.w, cs.z) - a);
+  if (br < smallest) {
+    smallest = br;
+    acc = cs.zw;
+  }
+  float bl = abs(atan(cs.w, cs.x) - a);
+  if (bl < smallest) {
+    smallest = bl;
+    acc = cs.xw;
+  }
+
+  return acc;
+}
+
+float angleFromTo(vec2 p1, vec2 p2) {
+  vec2 diff = p2 - p1;
+  return atan(diff.y, diff.x);
+}
+
+float diagonalLength(vec2 p) {
+  return sqrt(abs(p.x * p.x + p.y * p.y));
+}
+
+bool rectContains(vec4 rect, vec2 point) {
+  return rect.x <= point.x && point.x <= rect.z && rect.y <= point.y && point.y <= rect.w;
+}
+
+// TODO Glass: reflect and refract
+float castLightRay(vec2 origin, vec2 point) {
+  const int rectQuantity = ${rects};
+  const int maxDepth = 2;
+
+  float angle = angleFromTo(origin, point);
+  vec2 stateOrigin = origin;
+  vec2 statePoint = point;
+  float stateColor;
+
+  for (int depth = 0; depth < maxDepth; depth++) {
+    vec4 rect = vec4(-1.0);
+    float minDist = 2.0;
+    vec2 interPoint = vec2(-1.0);
+    float reflectionAngle = angle;
+
+    for (int i = 0; i < rectQuantity; i++) {
+      // We can probably restrict the lines in the rects to 2 instead of 4 using
+      // the angle of origin-point.
+      vec3 inter = lineRectIntersection(angle, stateOrigin, statePoint, rects[i]);
+
+      if (inter.x != -1.0) {
+        float distance = diagonalLength(statePoint - stateOrigin);
+
+        if (distance < minDist) {
+          interPoint = inter.xy;
+          reflectionAngle = inter.z;
+          rect = rects[i];
+          minDist = distance;
+        }
+      }
+    }
+
+    // Didn't hit anything lol
+    if (rect.x == -1.0) {
+      return 1.0 - diagonalLength(stateOrigin - statePoint);
+    }
+
+    bool isInsideRect = rectContains(rect, statePoint);
+
+    bool isRectReflective = false; // rect[4] > 0.0
+    if (isRectReflective && depth < maxDepth) {
+      angle = reflectionAngle;
+      stateOrigin = interPoint;
+      // stateColor = rect.color;
+      continue;
+    }
+
+    float strength = 1.0 - diagonalLength(stateOrigin - interPoint);
+
+    if (isInsideRect) {
+      return strength;
+    }
+
+    return 0.0;
+
+    vec2 start = vec2(interPoint.x + cos(angle) * 0.001, interPoint.y + sin(angle) * 0.001);
+    vec2 end = vec2(cos(angle) * 2.0, sin(angle) * 2.0);
+
+    // XXX We can use this to do diffuse lighting and this is how.txt:
+    // - We have inerPoint which is _outside_ the rect we're inside, so we can
+    //   easily reflect and check for other rects in that direction, and obtain
+    //   the colour of that rect, and return that mixed with original colour.
+    for (int i = 0; i < rectQuantity; i++) {
+      vec3 inter = lineRectIntersection(angle, start, end, rects[i]);
+
+      if (inter.x != -1.0) {
+        return 0.0;
+      }
+    }
+
+    // Point is illuminated
+    return strength;
+  }
+
+  return stateColor;
 }
 
 void main() {
   gl_FragColor = texture2D(uSampler, vTextureCoord);
 
-  float light = 1.0;
-  for (int i = 0; i < ${rects}; i++) {
-    for (int j = 0; j < ${lights}; j++) {
+  const int rectQuantity = ${rects};
+  const int lightQuantity = ${lights};
+
+  vec4 color = vec4(gl_FragColor.xyz * 0.1, gl_FragColor.w);
+
+  for (int i = 0; i < lightQuantity; i++) {
+    float brightness = castLightRay(lights[i], vTextureCoord);
+    color = max(color, gl_FragColor * brightness);
+  }
+
+  gl_FragColor = color;
+}
+
+// This is way more efficient but totally like less cool bro
+void notMain() {
+  gl_FragColor = texture2D(uSampler, vTextureCoord);
+
+  const int rectQuantity = ${rects};
+  const int lightQuantity = ${lights};
+
+  vec4 darkness = vec4(gl_FragColor.xyz * 0.1, gl_FragColor.w);
+  vec4 color = darkness;
+
+  for (int i = 0; i < rectQuantity; i++) {
+    vec4 subColor = darkness;
+    bool anyShaded = false;
+    bool allShaded = true;
+
+    for (int j = 0; j < lightQuantity; j++) {
       vec2 c = vTextureCoord - lights[j];
-      float r = sqrt(c.x * c.y);
+      float coordDistFromLight = diagonalLength(c);
+
+      float angleToRect = angleFromTo(lights[j], (rects[i].xy + rects[i].zw) * 0.5);
+      vec2 corner = closestVertexToAngle(rects[i], angleToRect);
+
+      float coordDistFromCorner = diagonalLength(corner - vTextureCoord.xy);
+      if (coordDistFromCorner < 0.01) {
+        subColor.g *= 1.5;
+      }
+
+      // So we now know the corner at which we can begin to shave off shadow.
+      // However, a problem persists, with two branches:
+      //  * How do we know if we're restricting shadow to below or above?
+      //  * Same as above, but with left or right.
+      //
+      // if sin(-ca) < 0 then vTextureCoord.y > corner.y else vTextureCoord.y < corner.y
+      // if cos(-ca) < 0 then vTextureCoord.x > corner.x else vTextureCoord.x < corner.x
+      //
+      // Above is out-of-date, "ca" is a bad angle do not use: use angleToRect
+
+      bool isFacingRay;
+      if (sin(angleToRect) > 0.0) {
+        isFacingRay = vTextureCoord.y <= corner.y;
+      } else {
+        isFacingRay = vTextureCoord.y > corner.y;
+      }
+      if (cos(angleToRect) > 0.0) {
+        isFacingRay = isFacingRay && vTextureCoord.x <= corner.x;
+      } else {
+        isFacingRay = isFacingRay && vTextureCoord.x > corner.x;
+      }
+
+      vec2 middle = (rects[i].zw + rects[i].xy) * 0.5;
+      float cornerDistFromLight = diagonalLength(middle - lights[j]);
+      bool isCoordInsideLightRadius = coordDistFromLight <= cornerDistFromLight;
 
       bool isInside = rects[i].x < vTextureCoord.x && rects[i].z > vTextureCoord.x && rects[i].y < vTextureCoord.y && rects[i].w > vTextureCoord.y;
-      if (vTextureCoord.y > rects[i].y && !isInside) {
+      if (!isInside) {
         vec4 p = rects[i] - vec4(lights[j].x, lights[j].y, lights[j].x, lights[j].y);
-        vec2 tas = vec2(atan(p.y, p.x), atan(p.y, p.z));
-        vec2 bas = vec2(atan(p.w, p.x), atan(p.w, p.z));
+
+        float apyx = atan(p.y, p.x);
+        float apyz = atan(p.y, p.z);
+        float apwz = atan(p.w, p.z);
+        float apwx = atan(p.w, p.x);
+
+        // There has to be a better way lol
+        vec2 tas = vec2(apyx, apyz);
+        vec2 bas = vec2(apwx, apwz);
+        vec2 ras = vec2(apyx, apwx);
+        vec2 las = vec2(apyz, apwz);
+
         float ma = (tas.x + tas.y + bas.x + bas.y) * 0.25;
 
         float ca = atan(c.y, c.x);
+        bool insideCone
+          = bas.x > ca && ca > bas.y
+          || tas.x > ca && ca > tas.y
+          || ras.x > ca && ca > ras.y
+          || las.x > ca && ca > las.y;
 
-        if (bas.x > ca && ca > bas.y || tas.x > ca && ca > tas.y) {
-          float yIntensity = 1.0 - r;
-          light = yIntensity;
+        if (isCoordInsideLightRadius && insideCone) {
+        } else if (insideCone) {
+          anyShaded = true || anyShaded;
         } else {
-          light = 2.0;
+          allShaded = false && allShaded;
         }
 
       } else if (isInside) {
         float l = rects[i].w - rects[i].y;
         float d = vTextureCoord.y - rects[i].y;
-        light = 1.0 - r * (d / l);
+        //subLight = 1.0 - r * (d / l);
       }
     }
+
+    if (anyShaded && allShaded) {
+      subColor = gl_FragColor * 0.5;
+    } else {
+      subColor += gl_FragColor - 0.001;
+    }
+
+    color = subColor;
   }
 
-  gl_FragColor *= light;
+  gl_FragColor = color;
 
-  if (light == 1.0) {
-    for (int i = 0; i < ${lights}; i++) {
-      vec2 d = abs(vTextureCoord - lights[i]);
-      float r = sqrt(d.x * d.y);
-      gl_FragColor *= 1.0 - r * 0.5;
-    }
+  for (int i = 0; i < lightQuantity; i++) {
+    float r = diagonalLength(vTextureCoord - lights[i]);
+    float antiBandingMagic = cos(vTextureCoord.x * 3141.0) * sin(vTextureCoord.y * 3141.0) * 0.005;
+    gl_FragColor *= 1.0 - r * 0.5 + antiBandingMagic;
   }
 }
 `
